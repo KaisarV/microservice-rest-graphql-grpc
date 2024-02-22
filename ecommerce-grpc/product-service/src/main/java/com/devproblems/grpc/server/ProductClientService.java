35e1979b317629e16fd9fe754787d7be7d453a9e
KaisarV <kaisar.valentino123@gmail.com>
2024-02-23 02:22:41 +0700

    product client service

--- ecommerce-grpc/product-service/src/main/java/com/devproblems/grpc/server/ProductClientService.java
+++ ecommerce-grpc/product-service/src/main/java/com/devproblems/grpc/server/ProductClientService.java
@@ -1,8 +1,7 @@
 package com.devproblems.grpc.server;
 
 
-import com.devProblems.Product;
-import com.devProblems.ProductServiceGrpc;
+import com.devProblems.*;
 import com.google.protobuf.Descriptors;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
@@ -31,90 +30,43 @@ public class ProductClientService {
         return productResponse.getAllFields();
     }
 
-    public Map<Descriptors.FieldDescriptor, Object> getAllProducts(int productId) {
-        Product productRequest = Product.newBuilder().setId(productId).build();
-        Product productResponse = synchronousClient.getProductById(productRequest);
+    public List<Map<Descriptors.FieldDescriptor, Object>> getAllProducts() throws InterruptedException {
+
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final List<Map<Descriptors.FieldDescriptor, Object>> response = new ArrayList<>();
+        asynchronousClient.getAllProducts(Empty.newBuilder().build(), new StreamObserver<Product>() {
+            @Override
+            public void onNext(Product product) {
+                response.add(product.getAllFields());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                countDownLatch.countDown();
+            }
+        });
+        boolean await = countDownLatch.await(1, TimeUnit.MINUTES);
+        return await ? response : Collections.emptyList();
+    }
+
+    public Map<Descriptors.FieldDescriptor, Object> saveProduct(ProductRequest productRequest) {
+        Product productResponse = synchronousClient.saveProduct(productRequest);
         return productResponse.getAllFields();
     }
 
+    public Map<Descriptors.FieldDescriptor, Object> saveOffer(Product productRequest) {
+        Product productResponse = synchronousClient.saveOffer(productRequest);
+        return productResponse.getAllFields();
+    }
 
+    public Map<Descriptors.FieldDescriptor, Object> addPrice(AddPriceRequest addPriceRequest) {
+        Product productResponse = synchronousClient.addPrice(addPriceRequest);
+        return productResponse.getAllFields();
+    }
 
-
-
-//    public List<Map<Descriptors.FieldDescriptor, Object>> getBooksByAuthor(int authorId) throws InterruptedException {
-//        final Author authorRequest = Author.newBuilder().setAuthorId(authorId).build();
-//        final CountDownLatch countDownLatch = new CountDownLatch(1);
-//        final List<Map<Descriptors.FieldDescriptor, Object>> response = new ArrayList<>();
-//        asynchronousClient.getBooksByAuthor(authorRequest, new StreamObserver<Book>() {
-//            @Override
-//            public void onNext(Book book) {
-//                response.add(book.getAllFields());
-//            }
-//
-//            @Override
-//            public void onError(Throwable throwable) {
-//                countDownLatch.countDown();
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                countDownLatch.countDown();
-//            }
-//        });
-//        boolean await = countDownLatch.await(1, TimeUnit.MINUTES);
-//        return await ? response : Collections.emptyList();
-//    }
-//
-//    public Map<String, Map<Descriptors.FieldDescriptor, Object>> getExpensiveBook() throws InterruptedException {
-//        final CountDownLatch countDownLatch = new CountDownLatch(1);
-//        final Map<String, Map<Descriptors.FieldDescriptor, Object>> response = new HashMap<>();
-//        StreamObserver<Book> responseObserver = asynchronousClient.getExpensiveBook(new StreamObserver<Book>() {
-//            @Override
-//            public void onNext(Book book) {
-//                response.put("Expensive Book", book.getAllFields());
-//            }
-//
-//            @Override
-//            public void onError(Throwable throwable) {
-//                countDownLatch.countDown();
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                countDownLatch.countDown();
-//            }
-//        });
-//        TempDB.getBooksFromTempDb().forEach(responseObserver::onNext);
-//        responseObserver.onCompleted();
-//        boolean await = countDownLatch.await(1, TimeUnit.MINUTES);
-//        return await ? response : Collections.emptyMap();
-//    }
-//
-//    public List<Map<Descriptors.FieldDescriptor, Object>> getBooksByGender(String gender) throws InterruptedException {
-//        final CountDownLatch countDownLatch = new CountDownLatch(1);
-//        final List<Map<Descriptors.FieldDescriptor, Object>> response = new ArrayList<>();
-//        StreamObserver<Book> responseObserver = asynchronousClient.getBooksByGender(new StreamObserver<Book>() {
-//            @Override
-//            public void onNext(Book book) {
-//                response.add(book.getAllFields());
-//            }
-//
-//            @Override
-//            public void onError(Throwable throwable) {
-//                countDownLatch.countDown();
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//                countDownLatch.countDown();
-//            }
-//        });
-//        TempDB.getAuthorsFromTempDb()
-//                .stream()
-//                .filter(author -> author.getGender().equalsIgnoreCase(gender))
-//                .forEach(author -> responseObserver.onNext(Book.newBuilder().setAuthorId(author.getAuthorId()).build()));
-//        responseObserver.onCompleted();
-//        boolean await = countDownLatch.await(1, TimeUnit.MINUTES);
-//        return await ? response : Collections.emptyList();
-//    }
 }
